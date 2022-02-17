7b21155092cda9deeabff3f3b710764cb4a577b4
nmemarcoding <nmemarcoding@gmail.com>
2022-02-17 09:42:07 -0800

    schema created

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+create table department(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+create table role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30)  NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
