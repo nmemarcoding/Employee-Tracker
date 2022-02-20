46c8192206934cab2e783d42db6f66ebedd7e084
nmemarcoding <nmemarcoding@gmail.com>
2022-02-19 20:02:57 -0800

    prompt added

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
 const inquirer = require('inquirer');
 const db = require('./db/conection.js')
+const consoleTable = require("console.table");
+
+function start() {
+    inquirer
+        .prompt([{
+            type: "list",
+            name: "todo",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+            ],
+        }, ])
+        .then((choice) => {
+            switch (choice.todo) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Add a department":
+                    addDepartment();
+                    break;
+
+                case "Add a role":
+                    addRole();
+                    break;
+
+                case "Add an employee":
+                    addEmployee();
+                    break;
+
+                case "Update an employee role":
+                    updateRole();
+                    break;
+            }
+        });
+}
+
+
+
+start();
