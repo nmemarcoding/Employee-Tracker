79012be818193f090ce00173169da6f8b9a3e2b6
nmemarcoding <nmemarcoding@gmail.com>
2022-02-19 20:06:33 -0800

    addDepartment added

--- index.js
+++ index.js
@@ -71,4 +71,44 @@ function viewAllRoles() {
     start();
 }
 
+// view all employees
+function viewAllEmployees() {
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`, (err, data) => {
+        console.log('\n')
+        console.table(data)
+    })
+    start();
+}
+
+// add a department
+function addDepartment() {
+    inquirer
+        .prompt({
+            type: "input",
+            message: "New department name:",
+            name: "new_department",
+        })
+        .then(function(answer) {
+            db.query(
+                    `INSERT INTO department SET ?`, {
+                        name: answer.new_department,
+                    },
+                    function(err, answer) {
+                        if (err) {
+                            throw err;
+                        }
+                    }
+                ),
+                console.log("New department added to database.");
+            console.log("\n");
+            console.table('New department', answer);
+            start();
+        });
+}
+
 start();
